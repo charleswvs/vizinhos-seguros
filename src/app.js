e1b547115d74c120fbf2987c957cf0435e40b4be
Charles Willian <wcharlesw15@gmail.com>
2019-12-26 12:50:49 -0300

    Configuração inicial

---
+++ src/app.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import routes from './routes';
+
+class App {
+  constructor(){
+    this.server = express();
+
+    this.middlewares();
+
+    this.routes();
+  }
+
+  middlewares(){
+
+  }
+
+  routes(){
+    this.server.use(routes);
+  }
+}
+
+export default new App().server;
