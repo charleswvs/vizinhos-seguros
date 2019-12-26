e9341c407cf3316ae8dc9e5e526d03315c0639d4
Charles Willian <wcharlesw15@gmail.com>
2019-12-26 13:57:28 -0300

    Adicionado sequelize e configuração de database

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import routes from './routes';
 
+import './database';
+
 class App {
-  constructor(){
+  constructor() {
     this.server = express();
 
     this.middlewares();
@@ -10,11 +12,9 @@ class App {
     this.routes();
   }
 
-  middlewares(){
+  middlewares() {}
 
-  }
-
-  routes(){
+  routes() {
     this.server.use(routes);
   }
 }
